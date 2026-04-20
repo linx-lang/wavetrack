ef38b6af5fedb1db2e78e1b254f94acb3cf5286f
linx-lang <linaissal2005@gmail.com>
2026-04-21 00:17:02 +0200

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Fichier contenant les constantes de connexions
-// A modifier si nécessaire
-define ('NOM', "root");
-define ('PASSE', "");
-define ('SERVEUR', "localhost");
-define ('BD', "projetsql");
-
-?>
