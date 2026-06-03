ea15b2cdbbef26f1d28eec397b85ed1b74a81a4b
linx-lang <linaissal2005@gmail.com>
2026-06-03 02:31:02 +0200

    Refactor InscriptionSalle.php for improved functionality

--- finale/InscriptionSalle.php
+++ finale/InscriptionSalle.php
@@ -28,7 +28,7 @@
                 die("SQL Error: " . mysqli_error($connexion));
             }
             $idSalle= mysqli_insert_id($connexion);
-            $_SESSION['idSalle'] = $idSalle; // Store the new ID in session for later use
+            $_SESSION['idSalle'] = $idSalle; 
 
             $joursSelectionnes = isset($_POST['jour']) ? $_POST['jour'] : [];// Récupère les jours sélectionnés ou un tableau vide
             $horairesDebut = isset($_POST['horaireD']) ? $_POST['horaireD'] : [];
@@ -36,13 +36,10 @@
 
             foreach ($joursSelectionnes as $jour) {
                 if (empty($jour)) continue;// Ignore les jours vides
+                $idJour = (int)$jour; // Convertit en entier pour la base de données
 
-                $query1 = "INSERT INTO jour(nom) VALUES ('$jour')";
-                mysqli_query($connexion, $query1);
-                $idJour = mysqli_insert_id($connexion);
-
-                $debs = $horairesDebut[$jour]? $horairesDebut[$jour] : [];
-                $fins = $horairesFin[$jour]? $horairesFin[$jour] : [];
+                $debs = $horairesDebut[$jour]? $horairesDebut[$jour] : [];// Récupère les horaires de début pour ce jour ou un tableau vide
+                $fins = $horairesFin[$jour]? $horairesFin[$jour] : [];// Récupère les horaires de fin pour ce jour ou un tableau vide
 
                 for ($i = 0; $i < count($debs); $i++) {
                     $deb = $debs[$i] ? $debs[$i] : '';
@@ -50,6 +47,7 @@
                     if (empty($deb) || empty($fin)) continue;
 
                     $query20= "INSERT INTO ouverture (idSalle, idJ, horaireDeb, horaireFin) VALUES ('$idSalle', '$idJour', '$deb', '$fin')";
+        
                     $result20 = mysqli_query($connexion, $query20);
                     if (!$result20) {
                         die("INSERT Error ouverture (idSalle, idJ): " . mysqli_error($connexion));
@@ -69,20 +67,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WaveTrack- Inscription</title>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-     <link rel="stylesheet" href="css/header.css">
-     <link rel="stylesheet" href="css/Inscription.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/Inscription.css">
+    <link rel="stylesheet" href="css/InscriptionZoneProduit.css">
+    <link rel="stylesheet" href="css/header.css">
 </head>
 <body>
     <nav class="navbar bg-dark text-white p-3 mb-4">
         <div class="logo">
-           <img src="img/1000038818.png" alt="Logo" class="logo-img">
+            <img src="img/rasp.png" alt="Logo" class="logo-img">
             <span class="nomSite">WaveTrack</span>
         </div>
         
         <ul class="nav-links">
             <li><a href="#">Accueil</a></li>
-            
+            <li><a href="#">Produits</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">À propos</a></li>
             <li><img src="img/user.png" alt="Profile" class="user-img"></li>
         </ul>
     </nav>
@@ -102,61 +104,60 @@
                     <form method="POST" action="InscriptionSalle.php" class="p-4 border">
                     
                         <div class="col-md-12 mb-3">
-                            <label for="nomSalle">Nom de la salle</label>
                             <input type="text" placeholder="Nom de la salle" class="form-control mb-2" name="nomSalle">
-                            <label for="capacité">Capacité</label>
                             <input type="text" placeholder="Capacité" class="form-control mb-2" name="capaciteSalle">
-                            <label for="Code Postal">Code Postal</label>
                             <input type="text" placeholder="Code Postal" class="form-control mb-2" name="codePosSalle">
-                            <label for="Adresse">Adresse</label>
                             <input type="text" placeholder="Adresse" class="form-control mb-2" name="adresseSalle">
+                            <textarea class="form-control mb-2" placeholder="Autre" name="autreSalle"></textarea>
+                            <textarea class="form-control mb-2" placeholder="Périodes de fermetures" name="periodeFermetureSalle"></textarea>
+                            
                         </div>
 
-                        <p class="titre-horaires">Horaires de la semaine</p>
-                        
+                        <p>Horaires de la semaine:</p>
+                
                         <div class="jour-ligne">
-                            <input type="checkbox" id="mardi" name="jour[]" value="Mardi">
-                            <label for="Lundi">Lundi</label>
+                            <input type="checkbox" id="lundi" name="jour[]" value=1>
+                            <label for="lundi">Lundi</label>
                             <div class = "info">
-                                <div class="horaireP" id="hmardi">
-                                    <input type="text" placeholder="Horaire deb" name="horaireD[Mardi][]">
-                                    <input type="text" placeholder="Horaire fin" name="horaireF[Mardi][]">
+                                <div class="horaireP" id="hlundi">
+                                    <input type="text" placeholder="Horaire deb" name="horaireD[1][]">
+                                    <input type="text" placeholder="Horaire fin" name="horaireF[1][]">
                                 </div>
                             </div>
-                            <button type="button" onclick="ajouterHoraire('mardi',this)">+ Ajouter horaire</button>
+                            <button type="button" onclick="ajouterHoraire('lundi',this)">+ Ajouter horaire</button>
                         </div>
 
                         <div class="jour-ligne">
-                            <input type="checkbox" id="mardi" name="jour[]" value="Mardi">
+                            <input type="checkbox" id="mardi" name="jour[]" value=2>
                             <label for="mardi">Mardi</label>
                             <div class = "info">
                                 <div class="horaireP" id="hmardi">
-                                    <input type="text" placeholder="Horaire deb" name="horaireD[Mardi][]">
-                                    <input type="text" placeholder="Horaire fin" name="horaireF[Mardi][]">
+                                    <input type="text" placeholder="Horaire deb" name="horaireD[2][]">
+                                    <input type="text" placeholder="Horaire fin" name="horaireF[2][]">
                                 </div>
                             </div>
                             <button type="button" onclick="ajouterHoraire('mardi',this)">+ Ajouter horaire</button>
                         </div>
 
                         <div class="jour-ligne">
-                            <input type="checkbox" id="mercredi" name="jour[]" value="Mercredi">
+                            <input type="checkbox" id="mercredi" name="jour[]" value=3>
                             <label for="mercredi">Mercredi</label>
                             <div class = "info" id="hmercredi">
                                 <div class="horaireP">
-                                    <input type="text" placeholder="Horaire deb" name="horaireD[Mercredi][]">
-                                    <input type="text" placeholder="Horaire fin" name="horaireF[Mercredi][]">
+                                    <input type="text" placeholder="Horaire deb" name="horaireD[3][]">
+                                    <input type="text" placeholder="Horaire fin" name="horaireF[3][]">
                                 </div>
                             </div>
                             <button type="button" onclick="ajouterHoraire('mercredi',this)">+ Ajouter horaire</button>
                         </div>
                 
                         <div class="jour-ligne">
-                            <input type="checkbox" id="jeudi" name="jour[]" value="Jeudi">
+                            <input type="checkbox" id="jeudi" name="jour[]" value=4>
                             <label for="jeudi">Jeudi</label>
                             <div class = "info" id="hjeudi">
                                 <div class="horaireP">
-                                    <input type="text" placeholder="Horaire deb " name="horaireD[Jeudi][]">
-                                    <input type="text" placeholder="Horaire fin" name="horaireF[Jeudi][]">
+                                    <input type="text" placeholder="Horaire deb " name="horaireD[4][]">
+                                    <input type="text" placeholder="Horaire fin" name="horaireF[4][]">
                                 </div>
                             </div>
                             <button type="button" onclick="ajouterHoraire('jeudi',this)">+ Ajouter horaire</button>
@@ -164,12 +165,12 @@
 
 
                         <div class="jour-ligne">
-                            <input type="checkbox" id="vendredi" name="jour[]" value="Vendredi">
+                            <input type="checkbox" id="vendredi" name="jour[]" value=5>
                             <label for="vendredi">Vendredi</label>
                             <div class = "info" id="hvendredi">
                                 <div class="horaireP">
-                                    <input type="text" placeholder="Horaire deb" name="horaireD[Vendredi][]">
-                                    <input type="text" placeholder="Horaire fin" name="horaireF[Vendredi][]">
+                                    <input type="text" placeholder="Horaire deb" name="horaireD[5][]">
+                                    <input type="text" placeholder="Horaire fin" name="horaireF[5][]">
                                 </div>
                             </div>
 
@@ -178,24 +179,24 @@
                 
 
                         <div class="jour-ligne">
-                            <input type="checkbox" id="samedi" name="jour[]" value="Samedi">
+                            <input type="checkbox" id="samedi" name="jour[]" value=6>
                             <label for="samedi">Samedi</label>
                             <div class = "info" id="hsamedi">
                                 <div class="horaireP">
-                                    <input type="text" placeholder="Horaire deb" name="horaireD[Samedi][]">
-                                    <input type="text" placeholder="Horaire fin" name="horaireF[Samedi][]">
+                                    <input type="text" placeholder="Horaire deb" name="horaireD[6][]">
+                                    <input type="text" placeholder="Horaire fin" name="horaireF[6][]">
                                 </div>
                             </div>
                             <button type="button" onclick="ajouterHoraire('samedi',this)">+ Ajouter horaire</button>
                         </div>
                 
                         <div class="jour-ligne">
-                            <input type="checkbox" id="dimanche" name="jour[]" value="Dimanche">
+                            <input type="checkbox" id="dimanche" name="jour[]" value=7>
                             <label for="dimanche">Dimanche</label>
                             <div class = "info" id="hdimanche">
                                 <div class="horaireP">
-                                    <input type="text" placeholder="Horaire deb" name="horaireD[Dimanche][]" >
-                                    <input type="text" placeholder="Horaire fin" name="horaireF[Dimanche][]" >
+                                    <input type="text" placeholder="Horaire deb" name="horaireD[7][]" >
+                                    <input type="text" placeholder="Horaire fin" name="horaireF[7][]" >
                                 </div>
                             </div>
                             <button type="button" onclick="ajouterHoraire('dimanche',this)">+ Ajouter horaire</button>
@@ -209,12 +210,10 @@
                         
 
     </main>
-     <footer>
-    <p>Kozlov-mercier Nina - Issa Lina - Akbaba Julia - Doumergue Louise - Girard Alexia -
-        <a href="Contactapres.html" class="contactfooter">Nos Contacts</a>
-    </p>
-</footer>
- <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="java/Inscription.js"></script>
+     <footer class="text-center py-4 mt-5 border-top">
+        <p>Kozlov-mercier Nina - Issa Lina - Akbaba Julia - Doumergue Louise - Girard Alexia - 
+        <a href="Contactavant.html">Nos Contacts</a></p>
+    </footer>
 </body>
 </html>
+<script src="../wavetrack/java/inscription.js"></script>
