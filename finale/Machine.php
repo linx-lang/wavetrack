88608c00fd5b0b9b7a33bc7fa636f234ef338ac6
louise-0 <louise.doumergue@toulouse.miage.fr>
2026-06-03 05:13:19 +0200

    Update Machine.php

--- finale/Machine.php
+++ finale/Machine.php
@@ -196,7 +196,6 @@ $clim = recupererClimatiseur($connexion, $idSalle);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/header.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
